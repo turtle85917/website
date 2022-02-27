a4d1ccc59f5c6ecbbc3f8b721e01e5e77caa1582
turtle85917 <siu85917@gmail.com>
2022-02-27 17:44:26 +0900

    Edit Website

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Switch as Router } from 'react-router-dom';
 import { Auth, Main } from './pages';
 
 function App() {
+  console.log(window.location)
   return (
     <Router>
       <Route exact path="/website/" component={Main} />
