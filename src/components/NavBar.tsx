8ca9154fb584b6b45a909beadf0b3b3e60970242
turtle85917 <siu85917@gmail.com>
2022-02-27 17:37:53 +0900

    Edit Website

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 class NavBar extends Component {
     render() {
@@ -13,7 +13,7 @@ class NavBar extends Component {
                         <a href="/website/">Home</a>
                     </li>
                     <li className="nav-item">
-                        <FontAwesomeIcon icon={faUserSlash} />
+                        <FontAwesomeIcon icon={faSignInAlt} />
                         <a href="https://discord.com/api/oauth2/authorize?client_id=947370255259410432&redirect_uri=https%3A%2F%2Fturtle85917.github.io%2Fwebsite%2Fauth&response_type=code&scope=identify%20email">Test</a>
                     </li>
                 </ul>
