1ae899d901bd8a089f09c2f107151aa9ca07ee51
turtle85917 <siu85917@gmail.com>
2022-02-27 09:18:09 +0900

    Edit website

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch as Router } from 'react-router-dom';
 
 import { Main } from './pages';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Route exact path="/" component={Main} />
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Route exact path="/" component={Main} />
+    </Router>
+  );
 }
 
 export default App;
