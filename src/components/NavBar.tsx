67d3cd672d1a412b575da02529c71789151292b5
turtle85917 <siu85917@gmail.com>
2022-02-27 13:55:34 +0900

    Edit Website

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 class NavBar extends Component {
     render() {
         return (
             <nav>
                 <li className="nav-item">
-                    <FontAwesomeIcon icon={"house"} />
+                    <FontAwesomeIcon icon={faHouse} />
                     <a href="/website/">Home</a>
                 </li>
 
